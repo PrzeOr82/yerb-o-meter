164ed1a6ff195a8e6a28c94fce31dd1bccf30749
Przemek Orechwa <przemyslaw.orechwa@gmail.com>
2019-03-15 16:18:43 +0100

    get btn id added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-let btn = document.querySelector('button');
+let btnAdd = document.getElementById('btnAdd');
 let addedYerba;
 let dropdown = document.querySelector('#dropdown');
 let option;
 let table = document.getElementById('lista');
 let list = [];
 let obj = {};
+let num = table.lastElementChild.getAttribute('id').replace('item', '') * 1;
+let btnRem;
 
 class Item {
     constructor(name, score) {
@@ -13,20 +15,29 @@ class Item {
     }
 }
 
+
+getBtnId = (btnList) => {
+    for (i = 0; i < btnList.length; i++) {
+        btnList[i].onclick = function(e) {
+            alert(this.id);
+        }
+    }
+}
+
 addItem = () => {
+    num++;
     addedYerba = document.querySelector('#addedYerba').value;
     option = dropdown[dropdown.selectedIndex].value;
 
     obj = new Item(addedYerba, option);
     list.push(obj);
 
-    table.insertAdjacentHTML('beforeEnd', `<tr><td>${obj.name}</td><td>${obj.score}</td><td><button class="btn" style="color:green"><i class="fas fa-pen"></i></button></td><td><button class="btn" style="color:red"><i class="fas fa-trash-alt"></i></button></td></tr>`);
+    table.insertAdjacentHTML('beforeEnd', `<tr id="item${num}"><td>${obj.name}</td><td>${obj.score}</td><td><button id="btnEdit${num}" class="btn" style="color:green"><i class="fas fa-pen"></i></button></td><td><button id="btnRem${num}" class="btn" style="color:red"><i class="fas fa-trash-alt"></i></button></td></tr>`);
     document.querySelector('#addedYerba').value = ''
     dropdown.selectedIndex = 0;
+    btnRem = document.querySelectorAll('button[id^="btnRem"]');
 
-
+    getBtnId(btnRem);
 }
 
-
-
-btn.addEventListener('click', addItem);
+btnAdd.addEventListener('click', addItem);
