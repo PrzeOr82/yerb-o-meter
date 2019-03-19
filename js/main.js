fcd91264ca6efb4801544a50289ccdfb105dc01b
Przemek Orechwa <przemyslaw.orechwa@gmail.com>
2019-03-19 11:29:50 +0100

    edycja działa :D

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ let list = [];
 let obj = {};
 let num = table.lastElementChild.getAttribute('id').replace('item', '') * 1;
 let btnRem;
+let btnEdit;
 let row;
+let dialog = document.querySelector('#editPopup');
+let btnCancel = document.getElementById('cancel');
+let btnSubmit = document.getElementById('submit');
+let editName = document.getElementById('editName');
+let editScore = document.getElementById('editScore');
+let idItem;
+
 
 class Item {
     constructor(name, score) {
@@ -16,11 +24,35 @@ class Item {
     }
 }
 
-btnAddRemFunction = (btnList) => {
-    for (i = 0; i < btnList.length; i++) {
-        btnList[i].onclick = function(e) {
-            row = document.getElementById('item' + this.id.replace('btnRem', ''));
+
+btnRemFunction = (btnRemList) => {
+    for (i = 0; i < btnRemList.length; i++) {
+        btnRemList[i].onclick = function() {
+            idItem = this.id.replace('btnRem', '');
+            row = document.getElementById('item' + idItem);
             row.remove();
+        }
+    }
+}
+
+editValues = (id) => {
+    editName.value = document.getElementById('name' + id).innerText;
+    editScore.value = document.getElementById('score' + id).innerText;
+}
+
+saveEditedValues = () => {
+    document.getElementById('name' + idItem).innerText = editName.value
+    document.getElementById('score' + idItem).innerText = editScore.value
+
+}
+
+btnEditFunction = (btnEditList) => {
+    for (i = 0; i < btnEditList.length; i++) {
+        btnEditList[i].onclick = function() {
+            // console.log(this.id);
+            dialog.showModal();
+            idItem = this.id.replace('btnEdit', '');
+            editValues(idItem);
         }
     }
 }
@@ -33,12 +65,20 @@ addItem = () => {
     obj = new Item(addedYerba, option);
     list.push(obj);
 
-    table.insertAdjacentHTML('beforeEnd', `<tr id="item${num}"><td>${obj.name}</td><td>${obj.score}</td><td><button id="btnEdit${num}" class="btn" style="color:green"><i class="fas fa-pen"></i></button></td><td><button id="btnRem${num}" class="btn" style="color:red"><i class="fas fa-trash-alt"></i></button></td></tr>`);
+    table.insertAdjacentHTML('beforeEnd', `<tr id="item${num}"><td id="name${num}">${obj.name}</td><td id="score${num}">${obj.score}</td><td><button id="btnEdit${num}" class="btn" style="color:green"><i class="fas fa-pen"></i></button></td><td><button id="btnRem${num}" class="btn" style="color:red"><i class="fas fa-trash-alt"></i></button></td></tr>`);
     document.querySelector('#addedYerba').value = ''
     dropdown.selectedIndex = 0;
     btnRem = document.querySelectorAll('button[id^="btnRem"]');
+    btnEdit = document.querySelectorAll('button[id^="btnEdit"]');
 
-    btnAddRemFunction(btnRem);
+    btnRemFunction(btnRem);
+    btnEditFunction(btnEdit);
 }
 
 btnAdd.addEventListener('click', addItem);
+
+btnCancel.addEventListener('click', function() {
+    dialog.close('animalNotChosen');
+});
+
+btnSubmit.addEventListener('click', saveEditedValues);
